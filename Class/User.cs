caa8c7831c24d97c2572eac64b34afe98c8c9f91
wesley <wesleyfaraujjo@gmail.com>
2023-12-14 20:52:12 -0300

    Creating the user class and starting the program

---
+++ Class/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student.Class
+{
+    internal class User(string name, string password, string email)
+    {
+        private string name = name;
+        private string password = password;
+        private string email = email;
+
+        public string Name
+        {  
+            get {  return name; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("The name cannot be empty!");
+                }   
+            }
+        }
+        public string Password 
+        {
+            get { return  password; }
+            set { password = value; }
+        }
+        public string Email
+        { 
+            get { return email; }
+            set { email = value; }
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Nome: {name}, Password: {password}, Email: {email}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Student.Class;
+
+class Programa
+{
+    static void Main()
+    {
+        string input;
+        List<User> users = new List<User>();
+
+        do
+        {
+            Console.WriteLine("*---------------------------------------*");
+            Console.WriteLine("Informe qual opção desejas: ");
+            Console.WriteLine("1 - Cadastro de usuário");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("*---------------------------------------*");
+
+            input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    string name;
+                    string password;
+                    string email;
+
+                    Console.WriteLine("Informe o nome: ");
+                    name = Console.ReadLine();
+
+                    Console.WriteLine("Informe a senha: ");
+                    password = Console.ReadLine();
+
+                    Console.WriteLine("Informe o E-mail: ");
+                    email = Console.ReadLine();
+
+                    User user = new(name, password, email);
+                    
+                    users.Add(user);
+                    break;
+
+                    default: 
+                    Console.WriteLine("Encerrando...");
+                    break;
+
+            }
+
+        } while (input != "0");
+    }
+}
